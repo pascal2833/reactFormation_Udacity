0a6bebac4ef8ba35a9ac6240526bae1a3782f337
pascal <pascal2833@gmail.com>
2018-12-20 14:51:40 +0100

    Add dynamic filter input

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,35 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts (props) {
-    return (
-        <ol className='contact-list'>
-            {props.contacts.map(contact => (
-                <li key={contact.id} className='contact-list-item'>
-                    <div
-                        className='contact-avatar'
-                        style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}
-                    ></div>
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.handle}</p>
-                    </div>
-                    <button
-                        onClick={() => props.onDeleteContact(contact)}
-                        className='contact-remove'>
-                        Remove
-                    </button>
-                </li>
-            ))}
-        </ol>
-    )
-}
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired,
+    }
+    state = {
+        query: ''
+    }
 
-ListContacts.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
+    updateQuery = (queryEnter) => {
+        this.setState (() => ({
+            query: queryEnter.trim()
+        }))
+    }
+
+    render () {
+        const { query } = this.state
+        const { contacts, onDeleteContact } = this.props
+        const showingContacts = query === ''
+            ? contacts
+            : contacts.filter((c) => c.name.toLowerCase().includes(query.toLowerCase()))
+        return (
+            <div className={'list-contacts'}>
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type="text"
+                        placeholder='Search contacts'
+                        value={query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+
+                <ol className='contact-list'>
+                    {showingContacts.map(contact => (
+                        <li key={contact.id} className='contact-list-item'>
+                            <div
+                                className='contact-avatar'
+                                style={{
+                                    backgroundImage: `url(${contact.avatarURL})`
+                                }}
+                            ></div>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button
+                                onClick={() => onDeleteContact(contact)}
+                                className='contact-remove'>
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
 }
 
 export default ListContacts
